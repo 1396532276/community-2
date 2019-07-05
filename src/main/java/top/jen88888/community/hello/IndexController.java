bb0e8846faeb152946e6136f5c5e70f001d99ea7
1396532276 <1396532276@qq.com>
2019-07-05 18:41:11 +0800

    说明

---
+++ src/main/java/top/jen88888/community/hello/IndexController.java
@@ -0,0 +1,14 @@
+package top.jen88888.community.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
